31622d0805405b432fdf0a84975c2b2e7cb15259
Nitin <142768664+nitin272@users.noreply.github.com>
2025-10-05 05:05:55 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL,
+  origin: ["http://localhost:4500","https://book-review-platform.pages.dev/"],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
